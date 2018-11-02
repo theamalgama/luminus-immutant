492faf2e104c083412efd266a57f8168b207e49d
Ezequiel R. Aguerre <ezequiel@amalgama.co>
2018-11-02 19:11:38 -0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/luminus-framework/luminus-immutant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.immutant/web "2.1.9"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.immutant/web "2.1.10"]])
